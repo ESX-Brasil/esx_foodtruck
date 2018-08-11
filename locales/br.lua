0185fb9c2aa125228836f9e1a6a0bdcb0ed41c9e
renildomarcio <krsolucoesweb@gmail.com>
2018-08-10 22:35:28 -0300

    Create br.lua

---
+++ locales/br.lua
@@ -0,0 +1,61 @@
+Locales['br'] = {
+  -- Cloakroom
+  ['vehicle_list'] = 'Lista de veículos',
+  ['civil'] = 'roupas civis',
+  ['job_clothes'] = 'roupas de trabalho',
+  ['foodtruck_actions_menu'] = 'pressione ~INPUT_CONTEXT~ para acessar o menu.',
+  
+  -- Vehicles
+  ['store_veh'] = 'pressione ~INPUT_CONTEXT~ para guardar o veículo ' ,
+  ['wrong_veh'] = 'Você tem que estar em um ~b~Foodtruck~s~.',
+  ['vehicles'] = 'veículos',
+
+  -- Menus
+  ['recipe'] = 'receita',
+  ['ingredients'] = 'Ingredientes',
+  ['price_unit'] = '$ / unidade',
+  ['on_you'] = 'em você',
+  ['action'] = 'ação',
+  ['buy_10'] = 'Comprar 10',
+  ['buy_50'] = 'Comprar 50',
+  ['cook'] = 'preparar',
+  ['billing'] = 'faturamento',
+  ['gears'] = 'material',
+  ['purchased'] = 'Compra feita.',
+
+  -- Boss Menu
+  ['boss_actions'] = 'ações de chefe',
+
+  -- Food
+  ['grill'] = 'grade',
+  ['table'] = 'mesa',
+  ['chair'] = 'cadeira',
+  ['clean'] = 'Limpar',
+  ['bread'] = 'pão',
+  ['meat'] = 'carne',
+  ['vegetables'] = 'legumes',
+  ['tacos'] = 'tacos',
+  ['burger'] = 'burger',
+  ['drank_coke'] = 'Você bebeu um ~b~Coke',
+  ['eat_burger'] = 'Você comeu um ~b~Burger',
+  ['eat_taco'] = 'Você comeu um ~b~Taco',
+  -- F6 Menu
+
+  -- Phone
+  ['alert_foodtruck'] = 'Alerta do cliente',
+  -- MISC
+  ['blip_market'] = 'mercado',
+  ['blip_foodtruck'] = 'foodtruck',
+  ['take'] = 'pressione ~INPUT_CONTEXT~ para pegar',
+  ['clean_too_far'] = 'você está muito longe de seu equipamento para guardá-lo.',
+  ['cooked'] = 'cozinhamento feito!',
+  ['no_player_nearby'] = 'Nenhum jogador por perto',
+  ['bill_amount'] = 'Valor da fatura',
+  ['missing_ingredients'] = 'você está perdendo ingredientes!',
+  ['already_cooking'] = 'uma receita já está em preparação!',
+  ['start_cooking_hint'] = 'pressione ~INPUT_CONTEXT~ para cozinhar.',
+  ['need_more_exp'] = 'Você não ~r~ tem ~s~ experiente o suficiente para executar essa ação.',
+  ['foodtruck_menu'] = 'pressione ~INPUT_CONTEXT~ to access menu.',
+  ['foodtruck_market_menu'] = 'pressione ~INPUT_CONTEXT~ para acessar o mercado.',
+  ['no_money'] = 'Você não tem dinheiro suficiente!',
+}
